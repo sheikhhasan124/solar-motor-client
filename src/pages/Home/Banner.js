c14b9aa3737cd43ac273e08d8b10085c6a7b37c6
hasan-ullah <mdhasanullah124@gmail.com>
2022-06-05 20:45:20 +0600

    change title name , icon

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -7,8 +7,8 @@ const Banner = () => {
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
           <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-            <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <h1 class="mb-5  lg:text-6xl text-5xl font-bold">RENEWABLE ENERGY</h1>
+            <p class="mb-5 text-xl font-bold">Make Natural Environment. Get Solar Energy. Remove co2. Make better future for our generation</p>
             <button class="btn btn-primary">Get Started</button>
           </div>
         </div>
